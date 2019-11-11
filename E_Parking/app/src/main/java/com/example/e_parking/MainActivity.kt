77ad80270c642252d98ef23388b5324bf35c4ba9
Samuel Eljoy <54344839+crimson-eclipse@users.noreply.github.com>
2019-11-12 02:07:37 +0700

    Login function done, layout login , home, dll.

--- E_Parking/app/src/main/java/com/example/e_parking/MainActivity.kt
+++ E_Parking/app/src/main/java/com/example/e_parking/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.e_parking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.home.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,7 +13,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btnLogin.setOnClickListener {
-            setContentView(R.layout.home)
+            val username = edtUsername.text.toString();
+            val password = edtPassword.text.toString();
+
+            if(username.isEmpty() || password.isEmpty()){
+                Toast.makeText(this, "Tolong Masukkan Email and Password", Toast.LENGTH_LONG).show()
+
+            }
+            if(username == "admin" && password == "admin"){
+
+                setContentView(R.layout.home)
+            }else{
+                Toast.makeText(this, "Email atau Password Salah", Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
