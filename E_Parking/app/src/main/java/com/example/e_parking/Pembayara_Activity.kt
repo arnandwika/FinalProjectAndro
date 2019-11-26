814e2093a1707d2cd03392c25ebb3f0660d2c55f
Arnan Dwika Diasmara <arnan.dwika@ti.ukdw.ac.id>
2019-11-26 11:51:13 +0700

    fix scan qr code

--- E_Parking/app/src/main/java/com/example/e_parking/Pembayara_Activity.kt
+++ E_Parking/app/src/main/java/com/example/e_parking/Pembayara_Activity.kt
@@ -1,12 +1,18 @@
 package com.example.e_parking
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_pembayara_.*
 
 class Pembayara_Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pembayara_)
+        kembali.setOnClickListener {
+            val i = Intent(baseContext, Home_Activity::class.java)
+            startActivity(i)
+        }
     }
 }
--- E_Parking/app/src/main/java/com/example/e_parking/scan_Activity.kt
+++ E_Parking/app/src/main/java/com/example/e_parking/scan_Activity.kt
@@ -1,13 +1,73 @@
 package com.example.e_parking
 
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Vibrator
 import android.provider.MediaStore
+import android.util.SparseArray
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import android.widget.TextView
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.vision.CameraSource
+import com.google.android.gms.vision.Detector
+import com.google.android.gms.vision.barcode.Barcode
+import com.google.android.gms.vision.barcode.BarcodeDetector
 
 class scan_Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan_)
+        val surfaceview: SurfaceView =findViewById(R.id.camerapreview)
+        val text: TextView =findViewById(R.id.textView)
+
+        var barcode: BarcodeDetector?=null
+        barcode =  BarcodeDetector.Builder(applicationContext).setBarcodeFormats(Barcode.QR_CODE).build()
+        val camerasource: CameraSource = CameraSource.Builder(this, barcode).setRequestedPreviewSize(640,480).build()
+
+        surfaceview!!.holder!!.addCallback(object: SurfaceHolder.Callback {
+            override fun surfaceChanged(p0: SurfaceHolder?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun surfaceDestroyed(p0: SurfaceHolder?) {
+                camerasource.stop()
+            }
+
+            override fun surfaceCreated(p0: SurfaceHolder?) {
+                if(ActivityCompat.checkSelfPermission(applicationContext,android.Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+                    return
+                }
+                camerasource.start(p0)
+            }
+
+        })
+        barcode.setProcessor(object: Detector.Processor<Barcode>{
+            override fun release() {
+
+            }
+
+            override fun receiveDetections(detections: Detector.Detections<Barcode>?) {
+                val qrCodes: SparseArray<Barcode>
+                qrCodes = detections!!.detectedItems
+                if(qrCodes.size()!=0){
+                    text.post(Runnable {
+                        kotlin.run {
+                            val vibrator: Vibrator
+                            vibrator=applicationContext.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+                            vibrator.vibrate(1000)
+                            text.setText(qrCodes.valueAt(0).displayValue)
+                            val i = Intent(baseContext, Pembayara_Activity::class.java)
+                            startActivity(i)
+                        }
+                    })
+                }
+            }
+
+        })
     }
 }
